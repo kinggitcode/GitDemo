87c171787ddbec99450e436e65e49e75f86a5f33
kanishk <iamkanishkazad@outlook.com>
2023-02-05 19:59:23 +0530

    first commit

---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,15 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	@Test
+	public void browwserAutomation() {
+		System.out.println("browserAutomation");
+	}
+
+	@Test
+	public void elementsUi() {
+		System.out.println("elementsUI");
+	}
+}
